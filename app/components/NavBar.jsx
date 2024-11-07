81666ac04f1646f4d88671cb92c31a7b58c52973
Jaiquez Hagood <west00brook@gmail.com>
2024-11-07 15:37:16 -0500

    intial commit

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -5,12 +5,13 @@ import Link from 'next/link'
 
 const NavBar = () => {
   return (
+
       <nav>
         <ul>
             <li><Link href="/home">Home</Link></li>
             <li><Link href="/work">Work</Link> 
                 <ul>
-                    <li><Link href="/work/bookblog">Book Blog</Link></li>
+                    <li><Link href="/work/bookblog">Book Blog help</Link></li>
                     <li><Link href="/work/l4d">GameSite</Link></li>
                     <li><Link href="/work/crocs">Crocs</Link></li> 
                     <li><Link href="/work//riddles">Riddles</Link></li> 
